c6bc004c0e981615d90cc4e0efbd4e92fb12bea6
Mergim <mergimberisha0@gmail.com>
2023-04-17 14:13:03 +0100

    Passing percentage_good method tests

--- 05_test_drive_a_class/lib/grammar_stats.rb
+++ 05_test_drive_a_class/lib/grammar_stats.rb
@@ -1,21 +1,31 @@
 class GrammarStats
-    def initialize(text)
-        @text = text
-    end
-  
-    def check(text)
-        fail "Not a sentence." if text.empty?
-        first_letter_is_uppercase = text[0] == text[0].upcase
-        last_character_is_valid = [".", "!", "?"].include? text[-1]
-        if first_letter_is_uppercase && last_character_is_valid
-            return true
-        else
-            return false
-        end
-    end
-  
-    def percentage_good
-      # Returns as an integer the percentage of texts checked so far that passed
-      # the check defined in the `check` method. The number 55 represents 55%.
+  def initialize(text)
+    @text = text
+    @correct_sentence_count = 0
+    @incorrect_sentence_count = 0
+  end
+
+  def check(text)
+    fail "Not a sentence." if text.empty?
+    first_letter_is_uppercase = text[0] == text[0].upcase
+    last_character_is_valid = [".", "!", "?"].include? text[-1]
+    if first_letter_is_uppercase && last_character_is_valid
+      @correct_sentence_count += 1
+      return true
+    else
+      @incorrect_sentence_count += 1
+      return false
     end
   end
+
+  def percentage_good
+    total = @correct_sentence_count + @incorrect_sentence_count
+    # if total == 0
+    #   return" kdd"
+    # end
+    percentage = @correct_sentence_count.to_f / total.to_f * 100
+    return percentage
+    # Returns as an integer the percentage of texts checked so far that passed
+    # the check defined in the `check` method. The number 55 represents 55%.
+  end
+end
--- 05_test_drive_a_class/spec/grammar_stats_spec.rb
+++ 05_test_drive_a_class/spec/grammar_stats_spec.rb
@@ -1,31 +1,34 @@
-require 'grammar_stats'
+require "grammar_stats"
 
 RSpec.describe GrammarStats do
-    
-    context "fails if given no text" do
-        it "fails" do
-            grammar_to_check = GrammarStats.new("")
-            expect { grammar_to_check.check("") }.to raise_error "Not a sentence."
-        end
+  context "fails if given no text" do
+    it "fails" do
+      grammar_to_check = GrammarStats.new("")
+      expect { grammar_to_check.check("") }.to raise_error "Not a sentence."
+    end
+  end
+
+  context "it checks whether the text has correct grammar" do
+    # text is a string
+    # Returns true or false depending on whether the text begins with a capital
+    # letter and ends with a sentence-ending punctuation mark.
+    it "returns true if text begins with a capital and ends with correct punctuation mark" do
+      grammar_to_check = GrammarStats.new("Hello there!")
+      result = grammar_to_check.check("Hello there!")
+      expect(result).to eq true
     end
 
-    context "it checks whether the text has correct grammar" do
-      # text is a string
-      # Returns true or false depending on whether the text begins with a capital
-      # letter and ends with a sentence-ending punctuation mark.
-        it "returns true if text begins with a capital and ends with correct punctuation mark" do
-            grammar_to_check = GrammarStats.new("Hello there!")
-            result = grammar_to_check.check("Hello there!")
-            expect(result).to eq true
-        end
-
-        it "returns false if text begins with a capital and ends with wrong punctuation mark" do
-            grammar_to_check = GrammarStats.new("Hello there!")
-            result = grammar_to_check.check("Hello there,")
-            expect(result).to eq false
-        end
-
+    it "returns false if text begins with a capital and ends with wrong punctuation mark" do
+      grammar_to_check = GrammarStats.new("Hello there!")
+      result = grammar_to_check.check("Hello there,")
+      expect(result).to eq false
     end
-
-
+  end
+  context "it calculates the percentage of text so far that pass" do
+    it "returns 100% if 100% of the text is correct" do
+      grammar_to_check = GrammarStats.new("Hello there!")
+      grammar_to_check.check("Hello there!")
+      expect(grammar_to_check.percentage_good).to eq 100
+    end
+  end
 end
