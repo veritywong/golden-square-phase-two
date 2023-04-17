eec7f18b9da0c0fd58bd5a24fb5783562a440b1f
Verity <veritywong90@gmail.com>
2023-04-17 11:49:15 +0100

    grammar stats first pass

---
+++ 05_test_drive_a_class/spec/grammar_stats_spec.rb
@@ -0,0 +1,11 @@
+require 'grammar_stats'
+
+RSpec.describe GrammarStats do
+    context "it checks whether the text has correct grammar" do
+        it "returns true if text begins with a capital and ends with correct punctuation mark" do
+
+        end
+    end
+
+
+end
