e298dee2e2c9b61af7868dff0c8d5bb3ac4e4ff1
Verity <veritywong90@gmail.com>
2023-04-17 16:49:45 +0100

    passing tests

--- 06_design_a_single_class_program/lib/track_tasks.rb
+++ 06_design_a_single_class_program/lib/track_tasks.rb
@@ -8,15 +8,13 @@ class TaskTracker
       @todo_list << task
     end
 
+    def completed(task)
+        @todo_list.delete(task)
+    end
+
     def see
       list = @todo_list.join(", ")
       "Your todo list is: #{list}"
     end
 
-    def completed(task)
-        list = @todo_list.join(", ")
-        if list(task)
-            list.delete!(task)
-        end
-    end
 end
